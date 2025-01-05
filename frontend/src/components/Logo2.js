abd44b066b2a2184f10cc273ba4af7e7f420a833
Zeenat <zeenatzfq052@gmail.com>
2025-01-06 02:15:24 +0530

    commit

---
+++ frontend/src/components/Logo2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo2 from "../assest/logo.png"
+
+const Logo = ({w,h}) => {
+  return (
+    <img src={logo2} width={w} height={h} alt='E Shop'/>
+  )
+}
+
+export default Logo
